aa7e7467d42c073d5baad078ac63fc328fe00786
John Kylle Santos <santosjohnkyllem@gmail.com>
2023-01-27 00:13:56 +0800

    Finished insertion sorting code

---
+++ Insertion-sort.py
@@ -0,0 +1,19 @@
+#assigned array 
+array = [11, 63, 49, 76, 47, 46, 59, 24, 45, 12]
+
+#function for determining the range of the array, we would start from 1 as the index in 0 will be the in the sorted list or the first digit to be compared to
+def insertion_sort(array):
+    length = range(1, len(array))
+
+    #function for getting every element in the array to be compared
+    for i in length:
+        #left value is the value in the left that would be compared to its right index
+        left_value = array[i]
+        #comparison while loop with swapping if the left index is greater that its right
+        while array[i-1] > left_value and i>0:
+            array[i], array[i-1] = array[i-1], array[i]
+            i = i-1
+
+    return array
+#print output
+print("Your Insertion sorted array is: ", insertion_sort(array))
